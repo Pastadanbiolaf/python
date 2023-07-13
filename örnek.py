ac1e598c7c361ff892d330a0f79e81ea1edca9e2
Pastadanbiolaf <139462361+Pastadanbiolaf@users.noreply.github.com>
2023-07-13 14:51:20 +0300

    Add files via upload

---
+++ örnek.py
@@ -0,0 +1,9 @@
+benimKahramanim = input("süper kahraman seçiniz:")
+if benimKahramanim == "Iron Man":
+    print("Iron Man karakterini RDJ oynamıştır.Özel gücü Tony Stark'ın kendi yaptığı demir kostumünde saklıdır uçabilir, ateş edebilir ve daha bir çok şeyi yapabilir.")
+elif benimKahramanim == "Captain America":
+    print("Captain America karakterini Chris Evans oynamıştır. Özel gücü ise kalkanında saklıdır.Üzgünüz C. Amerika hakkında pek bir bilgimiz yoktur.")
+elif benimKahramanim == "Spiderman":
+    print("Spiderman karakterini film serilerinde 3 aktor oynamıştır bu sebeple Marvel evreninde 3 karakter vardır: Tom Holland, Tobey Maguire, Andrew Garfield fakat animasyon olarak Miles'i de unutmamak gerekir. Bi örümceğin ısırığı ile ağ fırlatma, zıplama gibi örümcek güçlerine sahip olurlar. ")
+else:
+    print("Üzgünüz teknik bir hata nedeniyle cevap veremiyoruz")
